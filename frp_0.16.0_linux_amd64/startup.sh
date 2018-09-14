82a00803da1cf4ddd724a3a1b01d5867a1fc5165
sy <1530454315@qq.com>
2018-09-14 17:47:44 +0800

    2018-9-14 17:51:09

---
+++ frp_0.16.0_linux_amd64/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#setsid /home/sy/frp/frp_0.16.0_linux_amd64/frps -c /home/sy/frp/frp_0.16.0_linux_amd64/frps.ini
+setsid /home/sy/frp/frp_0.16.0_linux_amd64/frpc -c /home/sy/frp/frp_0.16.0_linux_amd64/frpc.ini
+
