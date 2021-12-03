b2e8e289a53e07f8b83af80ee27629a1c4208536
孙宇 <1530454315@qq.com>
2021-12-03 16:16:13 +0800

    Delete frp_0.16.0_linux_amd64 directory

--- frp_0.16.0_linux_amd64/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#setsid /home/sy/frp/frp_0.16.0_linux_amd64/frps -c /home/sy/frp/frp_0.16.0_linux_amd64/frps.ini
-setsid /home/sy/frp/frp_0.16.0_linux_amd64/frpc -c /home/sy/frp/frp_0.16.0_linux_amd64/frpc.ini
-
